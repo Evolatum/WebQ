32288625ac9af0e2bff755618d77a27b207e82a6
Omar Luna <mrebel62@gmail.com>
2019-10-24 20:05:17 -0500

    firebase key split

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
         signInSuccessUrl: './auth.html',
         signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
             // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
             firebase.auth.GithubAuthProvider.PROVIDER_ID,
